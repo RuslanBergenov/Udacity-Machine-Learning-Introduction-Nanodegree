cbcb23b50009834f03909385a1a0539f6e078c82
Ruslan Bergenov <Ruslan.Bergenov@gov.ab.ca>
2019-09-19 16:17:35 -0600

    1 Supervised Learning. Submitted Project 1 Finding Donors for CharityML

--- Supervised Learning/Project Fiding Donors for CharityML/Ruslan Bergenov CharityML/TEST CharityML.py
+++ Supervised Learning/Project Fiding Donors for CharityML/Ruslan Bergenov CharityML/TEST CharityML.py
@@ -133,6 +133,29 @@ print("{} total features after one-hot encoding.".format(len(encoded)))
 print (encoded)
 
 
+# =============================================================================
+# Shuffle and Split Data
+# =============================================================================
+# Import train_test_split
+try: 
+    # runs on my machine also in Udacity
+    from sklearn.model_selection import train_test_split
+except: 
+    # fails on my GOA machine:
+#    ModuleNotFoundError: No module named 'sklearn.cross_validation'. Seems to run in Udacity workspace, but gives a deprecation warning
+#     "This module will be removed in 0.20.", DeprecationWarning)
+    from sklearn.cross_validation import train_test_split
+    
+    
+
+# Split the 'features' and 'income' data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features_final, income, test_size = 0.2, random_state = 0)
+
+# Show the results of the split
+print("Training set has {} samples.".format(X_train.shape[0]))
+print("Testing set has {} samples.".format(X_test.shape[0]))
+
+
 
 
 
@@ -160,3 +183,105 @@ fscore = (1 + beta**2) * ((precision * recall) / (beta**2 * precision + recall))
 # Print the results 
 print("Naive Predictor: [Accuracy score: {:.4f}, F-score: {:.4f}]".format(accuracy, fscore))
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# TODO: Import two metrics from sklearn - fbeta_score and accuracy_score
+
+from sklearn.metrics import fbeta_score, accuracy_score
+
+def train_predict(learner, sample_size, X_train, y_train, X_test, y_test): 
+    '''
+    inputs:
+       - learner: the learning algorithm to be trained and predicted on
+       - sample_size: the size of samples (number) to be drawn from training set
+       - X_train: features training set
+       - y_train: income training set
+       - X_test: features testing set
+       - y_test: income testing set
+    '''
+    
+    results = {}
+    
+    # TODO: Fit the learner to the training data using slicing with 'sample_size' using .fit(training_features[:], training_labels[:])
+    start = time() # Get start time
+    learner.fit(X_train[:sample_size], y_train[:sample_size])
+            
+    end = time() # Get end time
+    
+    # TODO: Calculate the training time
+    results['train_time'] = end - start
+        
+    # TODO: Get the predictions on the test set(X_test),
+    #       then get predictions on the first 300 training samples(X_train) using .predict()
+    start = time() # Get start time
+    predictions_test = learner.predict(X_test)
+    predictions_train = learner.predict(X_train[:300])
+    end = time() # Get end time
+    
+    # TODO: Calculate the total prediction time
+    results['pred_time'] = end - start
+            
+    # TODO: Compute accuracy on the first 300 training samples which is y_train[:300]
+    results['acc_train'] = accuracy_score(y_train[:300], predictions_train)
+        
+    # TODO: Compute accuracy on test set using accuracy_score()
+    results['acc_test'] = accuracy_score(y_test, predictions_test)
+    
+    # TODO: Compute F-score on the the first 300 training samples using fbeta_score()
+
+    results['f_train'] = fbeta_score(y_train[:300], predictions_train, beta=0.5)
+        
+    # TODO: Compute F-score on the test set which is y_test
+    results['f_test'] = fbeta_score(y_test, predictions_test, beta=0.5)
+       
+    # Success
+    print("{} trained on {} samples.".format(learner.__class__.__name__, sample_size))
+        
+    # Return the results
+    return results
+
+
+
+
+
+
+# TODO: Import the three supervised learning models from sklearn
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier 
+from sklearn.svm import SVC
+
+
+# TODO: Initialize the three models
+clf_A = AdaBoostClassifier(random_state=777)
+clf_B = RandomForestClassifier(random_state=777)
+clf_C = SVC(random_state=777)
+
+# TODO: Calculate the number of samples for 1%, 10%, and 100% of the training data
+# HINT: samples_100 is the entire training set i.e. len(y_train)
+# HINT: samples_10 is 10% of samples_100 (ensure to set the count of the values to be `int` and not `float`)
+# HINT: samples_1 is 1% of samples_100 (ensure to set the count of the values to be `int` and not `float`)
+samples_100 = len(y_train)
+samples_10 = int(0.1* len(y_train))
+samples_1 = int(0.01* len(y_train))
+
+# Collect results on the learners
+results = {}
+for clf in [clf_A, clf_B, clf_C]:
+    clf_name = clf.__class__.__name__
+    results[clf_name] = {}
+    for i, samples in enumerate([samples_1, samples_10, samples_100]):
+        results[clf_name][i] = \
+        train_predict(clf, samples, X_train, y_train, X_test, y_test)
+
+# Run metrics visualization for the three supervised learning models chosen
+vs.evaluate(results, accuracy, fscore)
